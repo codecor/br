575b4402d68eb07d35fa72c08dc3d705af56f091
codecor <clcardio71@gmail.com>
2013-08-04 07:22:07 -0400

    basic usage now working. next, support for multiple files.

--- br.c
+++ br.c
@@ -13,28 +13,40 @@
 #include <time.h>
 #include <fcntl.h>
 
+// global storage file for files currently being carried (brought?) 
+char *tmp_file="/tmp/bringing";
+int DEBUG=1;
 
 main (int argc,char *argv[]){
     //printf("args: %i\narg[1]: %s\n",argc,argv[1]);
 
     // if one, store
     if (argc==2) {
-        char *tmp_file="tmp";
-        char file_name[]="2bring-file-addr";
+        char file_name[]="file not available";
         char ex_path[1024];
         char file_addr[1024];
         FILE *fp;
 
         // get file path
         getExePath(&ex_path);
-        //printf("[debug] ex_path = %s\n",ex_path);
+        if (DEBUG) printf("[debug] ex_path = %s\n",ex_path);
 
+        /* lets handle more than 1 file */
+        for (len
+        return 0;
         strcpy(file_name,argv[1]);
-        //printf("[debug] file_name = %s\n",file_name);
+        if (DEBUG) printf("[debug] file_name = %s\n",file_name);
 
-        //strcat(ex_path,"test");
-        strcat(ex_path,file_name);
-        strcpy(file_addr,ex_path);
+        if (!access(file_name,F_OK)) // file doesn't exist then give usage and exit
+        {
+            if (DEBUG) printf("file exists.\n");
+        } else {
+            if (DEBUG) printf("error: file does not exist.\n");
+            usage();
+            return 1;
+        }
+
+        sprintf(file_addr,strcat(ex_path,file_name));
         printf("[debug] file_addr=%s\n",file_addr);
 
          // check for file
@@ -50,8 +62,12 @@ main (int argc,char *argv[]){
     } else {
     // if none, drop
         // check for file waiting to be dropped
-        // error: no file to be dropped
-        printf("No file to be brought.\n");
+        if (bring_it()){
+            //sprintf(cmd,"cp %s .",
+        } else {
+            // error: no file to be dropped
+            printf("No file to be brought.\n");
+        }
     }
     //do_bring(argc,argv[1],argv[2]);
 
@@ -123,5 +139,48 @@ int getExePath(char *str){
 
     strcpy(str,path);
     //printf("[getExePath] str=%s\n",str);
+    return 1;
+}
+
+int bring_it(){
+    // read tmp file for main() to call do_copy with...
+    FILE *fp;
+    char line[300];
+    char cmd[300];
+    int status;
+
+    if ((fp=fopen(tmp_file,"r"))==NULL){
+        if (DEBUG) printf("cannot open %s\n",tmp_file);
+        return 0; 
+    } else {
+        while(!feof(fp)){
+            if(fgets(line,sizeof(line),fp)){
+                printf("copying file %s.\n",line);
+                sprintf(cmd,"/usr/bin/cp %s .",line);
+                if (DEBUG) printf("[d] [bi] [cmd] %s.\n",cmd);
+                status=system(cmd);
+                if (status != 0) 
+                    printf ("error: copy failed.\n");
+                else
+                {
+                    sprintf(cmd,"/usr/bin/rm %s",tmp_file);
+                    status=system(cmd);
+                    if (DEBUG) printf ("copy completed successfully\n");
+                }
+            }
+        }
+        fclose(fp);
+        return 1;
+    }
+}
+
+int usage(){
+    printf("*** br - bring ***\n## Overview\n Similar to cp, br copies files.");
+    printf("br will bring the files from point a to point b.\n## Usage\n ");
+    printf("When selecting files:\n  br <somefile>\n ... do what you gotta");
+    printf("do ... go where you gotta go; then to drop: \n  br\n");
+    printf("## Example\n [/root/]$ br main.c utils.c\n [/root/]$ cd ");
+    printf("development/\n [/root/development/]$ br");
+    printf("\n");
     return 1;
 }
